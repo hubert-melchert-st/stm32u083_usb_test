350fcd38e2794c1c29f022d449f72e4097fdca57
hubert_melchert <hubert.melchert@gmail.com>
2026-09-14 22:13:47 +0200

    Testing

---
+++ Debug/Middlewares/ST/usbx/common/usbx_stm32_device_controllers/ux_dcd_stm32_endpoint_destroy.d
@@ -0,0 +1,75 @@
+Middlewares/ST/usbx/common/usbx_stm32_device_controllers/ux_dcd_stm32_endpoint_destroy.o: \
+ ../Middlewares/ST/usbx/common/usbx_stm32_device_controllers/ux_dcd_stm32_endpoint_destroy.c \
+ ../Middlewares/ST/usbx/common/core/inc/ux_api.h \
+ ../Middlewares/ST/usbx/ports/generic/inc/ux_port.h ../USBX/App/ux_user.h \
+ ../Middlewares/ST/usbx/common/core/inc/ux_utility.h \
+ ../Middlewares/ST/usbx/common/core/inc/ux_system.h \
+ ../Middlewares/ST/usbx/common/usbx_stm32_device_controllers/ux_dcd_stm32.h \
+ ../USBX/Target/ux_stm32_config.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal.h \
+ ../Core/Inc/stm32u0xx_hal_conf.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_rcc.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32U0xx/Include/stm32u0xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32U0xx/Include/stm32u083xx.h \
+ ../Drivers/CMSIS/Include/core_cm0plus.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32U0xx/Include/system_stm32u0xx.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_rcc_ex.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_gpio.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_gpio_ex.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_dma.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_ll_dma.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_ll_dmamux.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_dma_ex.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_cortex.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_exti.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_flash.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_flash_ex.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pwr.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pwr_ex.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pcd.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_ll_usb.h \
+ ../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pcd_ex.h \
+ ../Middlewares/ST/usbx/common/core/inc/ux_device_stack.h
+../Middlewares/ST/usbx/common/core/inc/ux_api.h:
+../Middlewares/ST/usbx/ports/generic/inc/ux_port.h:
+../USBX/App/ux_user.h:
+../Middlewares/ST/usbx/common/core/inc/ux_utility.h:
+../Middlewares/ST/usbx/common/core/inc/ux_system.h:
+../Middlewares/ST/usbx/common/usbx_stm32_device_controllers/ux_dcd_stm32.h:
+../USBX/Target/ux_stm32_config.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal.h:
+../Core/Inc/stm32u0xx_hal_conf.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_rcc.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32U0xx/Include/stm32u0xx.h:
+../Drivers/CMSIS/Device/ST/STM32U0xx/Include/stm32u083xx.h:
+../Drivers/CMSIS/Include/core_cm0plus.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32U0xx/Include/system_stm32u0xx.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_rcc_ex.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_gpio.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_gpio_ex.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_dma.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_ll_dma.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_ll_dmamux.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_dma_ex.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_cortex.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_exti.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_flash.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_flash_ex.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pwr.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pwr_ex.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pcd.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_ll_usb.h:
+../Drivers/STM32U0xx_HAL_Driver/Inc/stm32u0xx_hal_pcd_ex.h:
+../Middlewares/ST/usbx/common/core/inc/ux_device_stack.h:
---
+++ Debug/USBX/App/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USBX/App/app_usbx_device.c \
+../USBX/App/ux_device_cdc_acm.c \
+../USBX/App/ux_device_descriptors.c 
+
+OBJS += \
+./USBX/App/app_usbx_device.o \
+./USBX/App/ux_device_cdc_acm.o \
+./USBX/App/ux_device_descriptors.o 
+
+C_DEPS += \
+./USBX/App/app_usbx_device.d \
+./USBX/App/ux_device_cdc_acm.d \
+./USBX/App/ux_device_descriptors.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+USBX/App/%.o USBX/App/%.su USBX/App/%.cyclo: ../USBX/App/%.c USBX/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUX_INCLUDE_USER_DEFINE_FILE -DUSE_HAL_DRIVER -DSTM32U083xx -c -I../USBX/App -I../USBX/Target -I../Core/Inc -I../Drivers/STM32U0xx_HAL_Driver/Inc -I../Drivers/STM32U0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32U0xx/Include -I../Middlewares/ST/usbx/common/core/inc -I../Middlewares/ST/usbx/ports/generic/inc -I../Middlewares/ST/usbx/common/usbx_stm32_device_controllers -I../Middlewares/ST/usbx/common/usbx_device_classes/inc -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-USBX-2f-App
+
+clean-USBX-2f-App:
+	-$(RM) ./USBX/App/app_usbx_device.cyclo ./USBX/App/app_usbx_device.d ./USBX/App/app_usbx_device.o ./USBX/App/app_usbx_device.su ./USBX/App/ux_device_cdc_acm.cyclo ./USBX/App/ux_device_cdc_acm.d ./USBX/App/ux_device_cdc_acm.o ./USBX/App/ux_device_cdc_acm.su ./USBX/App/ux_device_descriptors.cyclo ./USBX/App/ux_device_descriptors.d ./USBX/App/ux_device_descriptors.o ./USBX/App/ux_device_descriptors.su
+
+.PHONY: clean-USBX-2f-App
+
